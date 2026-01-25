5e126910c2abed1f75a32b1641bd265e3666f1e7
Zayd Abdulla <zaydabdulla2003@gmail.com>
2026-01-25 14:11:38 +0530

    Initial data loading and cleaning of packaging history dataset

---
+++ src/data_understanding.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+# Load datasets
+materials_df = pd.read_excel("data/materials_database_600.xlsx")
+history_df = pd.read_excel("data/real_packaging_history.xlsx")
+
+# View first rows
+print(materials_df.head())
+print(history_df.head())
+
+# =========================
+# DATA CLEANING - STEP 1
+# =========================
+
+print("\nMissing values in history dataset:")
+print(history_df.isnull().sum())
+
+
+# =========================
+# DATA CLEANING - STEP 2
+# Handle Missing Values
+# =========================
+
+# Fill missing numerical values with mean
+numerical_cols = [
+    'Distance_km',
+    'Cost_USD',
+    'CO2_Emission_kg'
+]
+
+
+for col in numerical_cols:
+    history_df[col].fillna(history_df[col].mean(), inplace=True)
+
+# Fill missing categorical values with mode
+history_df['Packaging_Used'].fillna(
+    history_df['Packaging_Used'].mode()[0], inplace=True
+)
+
+print("\nMissing values after cleaning:")
+print(history_df.isnull().sum())
